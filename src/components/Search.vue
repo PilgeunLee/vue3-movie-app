605e6e9114fc05a66c582fba1bb72c0d0eb9beb2
PilgeunLee <pgstom9015@gmail.com>
2024-02-01 22:46:11 +0900

    20240201

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,8 @@
         v-model="title"
         class="form-control"
         type="text"
-        placeholder="Search for Movies, Series & more" />
+        placeholder="Search for Movies, Series & more" 
+        @keyup.enter="apply" />
 
         <div class="selects"> 
             <select 
@@ -24,11 +25,18 @@
             </option>
             </select>
         </div>
+        <button
+            class="btn btn-primary"
+            @click="apply">
+            Apply
+        </button>
     </div>
 </template>
 
 <script>
-    export default {
+import axios from 'axios'
+
+export default {
         data() {
             return {
                 title:'',
@@ -58,8 +66,16 @@
                 ]
 
             }
+        },
+        methods:{
+           async apply() { 
+                const OMDB_API_KEY = '7035c60c'
+                const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
+                console.log(res)
+            }
         }
     }
+
 </script>
 
 <style lang="scss" scoped>
@@ -70,19 +86,24 @@
     font-size: 15px;
     &:last-child{   
         margin-right:0px;
-
-    }
-}
-}
-.selects{
-    display: flex;
-    select {
-        width : 120px;
-        margin-right :10px;
-        &:last-child{
-            margin-right: 0px;
         }
     }
-}
 
+    .selects{
+        display: flex;
+        select {
+            width : 120px;
+            margin-right :10px;
+            &:last-child{
+                 margin-right: 0px;
+            }
+        }
+    }
+    .btn{
+        width:120px;
+        height:50px;
+        font-weight: 700;
+        flex-shrink: 0;
+    }
+}
 </style>
