38f35f196d8381fe3bcf9b3f872c7a4b2767ec2b
PilgeunLee <pgstom9015@gmail.com>
2024-02-03 14:46:36 +0900

    vuex추가

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -34,7 +34,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
         data() {
@@ -69,9 +69,15 @@ export default {
         },
         methods:{
            async apply() { 
-                const OMDB_API_KEY = '7035c60c'
-                const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
-                console.log(res)
+                this.$store.dispatch('movie/searchMovies',{
+                    title: this.title,
+                    type: this.type,
+                    number: this.number,
+                    year: this.year
+                })
+                // const OMDB_API_KEY = '7035c60c'
+                // const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
+                // console.log(res)
             }
         }
     }
